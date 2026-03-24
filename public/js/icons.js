c5f740d436da6693695a5516c9c0b227f01cbfd8
millotcamilo-collab <millotcamilo@gmail.com>
2026-03-24 12:26:09 -0300

    Update icons.js

--- public/js/icons.js
+++ public/js/icons.js
@@ -7,21 +7,17 @@ window.ICONS = {
     DIAMOND: "/assets/icons/dia40.gif",
     CLUB: "/assets/icons/tre40.gif"
   },
-
   actions: {
     edit: "/assets/icons/desarrollo40.gif",
     save: "/assets/icons/salvar40.gif",
     delete: "/assets/icons/papelera30.gif",
-
     start: "/assets/icons/reloj60.gif",
     end: "/assets/icons/Campana80.gif",
     location: "/assets/icons/LocGlobito.gif",
-
     deadline: "/assets/icons/META60.gif",
     bomb: "/assets/icons/bombaRedonda60.gif",
-
     stop: "/assets/icons/stop60.gif",
-    boom: "/assets/icons/Boom80.gif"
+    boom: "/assets/icons/Boom80.gif",
     approve: "/assets/icons/Sello40.gif"
   }
 };
