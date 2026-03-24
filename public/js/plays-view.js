d4f4723e21906a30368dd3d96a41266cb50e97a5
millotcamilo-collab <millotcamilo@gmail.com>
2026-03-24 10:30:46 -0300

    Update plays-view.js

--- public/js/plays-view.js
+++ public/js/plays-view.js
@@ -1,3 +1,25 @@
+const ICONS = window.ICONS || {
+  suits: {
+    HEART: "/assets/icons/cor40.gif",
+    SPADE: "/assets/icons/pik40.gif",
+    DIAMOND: "/assets/icons/dia40.gif",
+    CLUB: "/assets/icons/tre40.gif"
+  },
+  actions: {
+    edit: "/assets/icons/desarrollo40.gif",
+    save: "/assets/icons/salvar40.gif",
+    delete: "/assets/icons/papelera30.gif",
+    start: "/assets/icons/reloj60.gif",
+    end: "/assets/icons/Campana80.gif",
+    location: "/assets/icons/LocGlobito.gif",
+    deadline: "/assets/icons/META60.gif",
+    bomb: "/assets/icons/bombaRedonda60.gif",
+    stop: "/assets/icons/stop60.gif",
+    boom: "/assets/icons/Boom80.gif",
+    approve: "/assets/icons/Sello40.gif"
+  }
+};
+
 let currentSuitFilter = null;
 let lastDeck = null;
 let lastPlays = [];
