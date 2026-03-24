9bc5f89547cb341513b45f5c225d8fbcb3013fc2
millotcamilo-collab <millotcamilo@gmail.com>
2026-03-24 09:50:29 -0300

    Create icons.js

---
+++ public/js/icons.js
@@ -0,0 +1,24 @@
+const ICONS = {
+  suits: {
+    HEART: "/assets/icons/cor40.gif",
+    SPADE: "/assets/icons/pik40.gif",
+    DIAMOND: "/assets/icons/dia40.gif",
+    CLUB: "/assets/icons/tre40.gif"
+  },
+
+  actions: {
+    edit: "/assets/icons/desarrollo40.gif",
+    save: "/assets/icons/salvar40.gif",
+    delete: "/assets/icons/papelera30.gif",
+
+    start: "/assets/icons/reloj60.gif",     // fecha inicio
+    end: "/assets/icons/Campana80.gif",     // fecha fin
+    location: "/assets/icons/LocGlobito.gif",
+
+    deadline: "/assets/icons/META60.gif",   // bandera
+    bomb: "/assets/icons/bombaRedonda60.gif",
+
+    stop: "/assets/icons/stop60.gif",
+    boom: "/assets/icons/Boom80.gif"
+  }
+};
