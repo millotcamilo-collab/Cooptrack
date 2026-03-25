babd7c4f44431b3d0cd41cbaa8800db29a2fba75
millotcamilo-collab <millotcamilo@gmail.com>
2026-03-25 14:39:48 -0300

    Create j-renderer.js

---
+++ public/js/renderers/j-renderer.js
@@ -0,0 +1,19 @@
+const {
+  escapeHTML,
+  formatDate,
+  getPlaySuit,
+  getPlayText,
+  getPlayAmount,
+  getPlayStartDate,
+  getPlayEndDate,
+  getSpadeMode,
+  getPlayLocation,
+  isApproved,
+  isChildPlay,
+  buildRecurrencePanel,
+  buildRecurrenceMarker,
+  buildIconButton,
+  buildSuitBadge,
+  buildApproveButton,
+  buildApprovedMeta
+} = window.PlayUIHelpers;
