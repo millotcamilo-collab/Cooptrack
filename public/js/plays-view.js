5aea3daa72c45a84f571c35b663ef5d1a956e683
millotcamilo-collab <millotcamilo@gmail.com>
2026-03-25 15:03:41 -0300

    Update plays-view.js

--- public/js/plays-view.js
+++ public/js/plays-view.js
@@ -1,32 +1,4 @@
-const ICONS = window.ICONS || {
-  suits: {
-    HEART: "/assets/icons/cor40.gif",
-    SPADE: "/assets/icons/pik40.gif",
-    DIAMOND: "/assets/icons/dia40.gif",
-    CLUB: "/assets/icons/tre40.gif"
-  },
-  actions: {
-    edit: "/assets/icons/desarrollo40.gif",
-    save: "/assets/icons/salvar40.gif",
-    delete: "/assets/icons/papelera30.gif",
-    start: "/assets/icons/reloj60.gif",
-    end: "/assets/icons/Campana80.gif",
-    location: "/assets/icons/LocGlobito.gif",
-    deadline: "/assets/icons/META60.gif",
-    bomb: "/assets/icons/bombaRedonda60.gif",
-    stop: "/assets/icons/stop60.gif",
-    boom: "/assets/icons/Boom80.gif",
-    approve: "/assets/icons/Sello40.gif",
-    exit: "/assets/icons/exit40.gif",
-    cancel: "/assets/icons/stop60.gif",
-    routine: "/assets/icons/ActividadIterativa80.gif",
-    fired: "/assets/icons/pistola60.gif",
-    send: "/assets/icons/buzon60.gif",
-    register: "/assets/icons/lacre80.gif",
-    reject: "/assets/icons/stepback40.gif",
-    quit: "/assets/icons/step60.gif"
-  }
-};
+const ICONS = window.ICONS;
 
 let currentSuitFilter = null;
 let lastDeck = null;
